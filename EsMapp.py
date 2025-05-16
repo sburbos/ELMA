a37ebdd0068395799e97833a1ef19287773efc0e
Leyley/Elley <61378312+sburbos@users.noreply.github.com>
2025-05-16 17:32:41 +0800

    Add files via upload

--- EsMapp.py
+++ EsMapp.py
@@ -15,7 +15,7 @@ st.set_page_config(
     page_icon=":writing_hand:",
     layout="wide"
 )
-st.logo("logo 1.png", icon_image="logo 3.png")
+st.logo("logo 1.png", icon_image="logo 4.png")
 def main_page():
     st.markdown("""
     <style>
